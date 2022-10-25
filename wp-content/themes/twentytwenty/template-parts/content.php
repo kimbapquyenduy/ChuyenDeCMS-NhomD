562a11ac2d9400dcdaddd9d6e090c92b1868da15
cucdaucho@gmail.com <cucdaucho@gmail.com>
2022-10-25 22:11:40 +0700

    commit module 2

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The default template for displaying content
  *
@@ -10,85 +11,179 @@
  * @subpackage Twenty_Twenty
  * @since Twenty Twenty 1.0
  */
-
+$class = '';
+if (!is_single()) {
+	$class = 'main-content-module2';
 ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" style="margin-top: 20px;">
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<?php
 
-	get_template_part( 'template-parts/entry-header' );
+				<article <?php post_class($class); ?> id="post-<?php the_ID(); ?>">
+					<div class="row">
+						<div class="col-md-3">
+							<?php
+							twentytwenty_the_post_meta(get_the_ID(), 'single-top');
+							?>
+						</div>
+						<div class="col-md-9 line-ver">
+							<?php
 
-	if ( ! is_search() ) {
-		get_template_part( 'template-parts/featured-image' );
-	}
+							get_template_part('template-parts/entry-header');
 
-	?>
+							if (!is_search()) {
+								get_template_part('template-parts/featured-image');
+							}
 
-	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
+							?>
 
-		<div class="entry-content">
+							<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
-			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
-				the_excerpt();
-			} else {
-				the_content( __( 'Continue reading', 'twentytwenty' ) );
-			}
-			?>
+								<div class="entry-content">
 
-		</div><!-- .entry-content -->
+									<?php
+									if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+										the_excerpt();
+									} else {
+										if (is_single()) {
+											the_content(__('Continue reading', 'twentytwenty'));
+										} else {
+											$post = get_post();
+											echo substr($post->post_content, 0, 200) . '<a style="color:teal;" href="' . esc_url(get_permalink()) . '">[..]</a>';
+										}
+									}
+									?>
 
-	</div><!-- .post-inner -->
+								</div><!-- .entry-content -->
 
-	<div class="section-inner">
-		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
-				'after'       => '</nav>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
+							</div><!-- .post-inner -->
 
-		edit_post_link();
 
-		// Single bottom post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+							<div class="section-inner">
+								<?php
+								wp_link_pages(
+									array(
+										'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+										'after'       => '</nav>',
+										'link_before' => '<span class="page-number">',
+										'link_after'  => '</span>',
+									)
+								);
 
-		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+								edit_post_link();
 
-			get_template_part( 'template-parts/entry-author-bio' );
+								// Single bottom post meta.
+								twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
 
-		}
-		?>
+								if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
 
-	</div><!-- .section-inner -->
+									get_template_part('template-parts/entry-author-bio');
+								}
+								?>
 
-	<?php
+							</div><!-- .section-inner -->
 
-	if ( is_single() ) {
 
-		get_template_part( 'template-parts/navigation' );
 
-	}
+							<?php
 
-	/*
+							if (is_single()) {
+
+								get_template_part('template-parts/navigation');
+							}
+
+							/*
 	 * Output comments wrapper if it's a post, or if comments are open,
 	 * or if there's a comment number – and check for password.
 	 */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
+							if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) {
+							?>
+
+								<div class="comments-wrapper section-inner">
+
+									<?php comments_template(); ?>
+
+								</div><!-- .comments-wrapper -->
+
+							<?php
+							}
+							?>
+						</div>
+					</div>
+
+				</article> <!-- .post -->
+
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+<?php } else { ?>
+	<article <?php post_class($class); ?> id="post-<?php the_ID(); ?>">
+		<?php
+		get_template_part('template-parts/entry-header');
+
+		if (!is_search()) {
+			get_template_part('template-parts/featured-image');
+		}
+
 		?>
+		<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
-		<div class="comments-wrapper section-inner">
+			<div class="entry-content">
 
-			<?php comments_template(); ?>
+				<?php
+				if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
+					the_excerpt();
+				} else {
+					if (is_single()) {
+						the_content(__('Continue reading', 'twentytwenty'));
+					} else {
+						$post = get_post();
+						echo substr($post->post_content, 0, 100);
+					}
+				}
+				?>
 
-		</div><!-- .comments-wrapper -->
+			</div><!-- .entry-content -->
+
+		</div><!-- .post-inner -->
+		<div class="section-inner">
+			<?php
+			wp_link_pages(
+				array(
+					'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__('Page', 'twentytwenty') . '"><span class="label">' . __('Pages:', 'twentytwenty') . '</span>',
+					'after'       => '</nav>',
+					'link_before' => '<span class="page-number">',
+					'link_after'  => '</span>',
+				)
+			);
+			edit_post_link();
+			// Single bottom post meta.
+			twentytwenty_the_post_meta(get_the_ID(), 'single-bottom');
+			if (post_type_supports(get_post_type(get_the_ID()), 'author') && is_single()) {
+				get_template_part('template-parts/entry-author-bio');
+			}
+			?>
+
+		</div><!-- .section-inner -->
 
 		<?php
-	}
-	?>
 
-</article><!-- .post -->
+		if (is_single()) {
+
+			get_template_part('template-parts/navigation');
+		}
+
+		if ((is_single() || is_page()) && (comments_open() || get_comments_number()) && !post_password_required()) { ?>
+			<div class="comments-wrapper section-inner">
+				<?php comments_template(); ?>
+			</div><!-- .comments-wrapper -->
+
+		<?php
+		}
+		?>
+	</article><!-- .post -->
+<?php } ?>
