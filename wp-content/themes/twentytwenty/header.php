f2f3cfc3a5bccd4e05e53e78d614d82f8adbfe43
cucdaucho@gmail.com <cucdaucho@gmail.com>
2022-10-27 21:51:01 +0700

    last commit module 1

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -28,6 +28,7 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/module1css.css" type="text/css" media="screen" />
 
 
 	<?php wp_head(); ?>
