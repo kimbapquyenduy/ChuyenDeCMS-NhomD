d4bc0f252ebbfa5e292c11f9854ca62668f0fbdc
cucdaucho@gmail.com <cucdaucho@gmail.com>
2022-10-29 00:10:30 +0700

    Revert "Merge branch 'master' into 08-LeQuyenDuy/module3"
    This reverts commit c73b3a07ca09637bb02f5acacee7a33d61ce916f, reversing
    changes made to ca7c31bd20ae04e303b34ae7343e289a25f95b42.

--- wp-content/themes/twentytwenty/template-parts/modal-menu.php
+++ wp-content/themes/twentytwenty/template-parts/modal-menu.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Displays the menu icon and modal
  *
@@ -19,8 +18,8 @@
 			<div class="menu-top">
 
 				<button class="toggle close-nav-toggle fill-children-current-color" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" data-set-focus=".menu-modal">
-					<span class="toggle-text"><?php _e('Close Menu', 'twentytwenty'); ?></span>
-					<?php twentytwenty_the_theme_svg('cross'); ?>
+					<span class="toggle-text"><?php _e( 'Close Menu', 'twentytwenty' ); ?></span>
+					<?php twentytwenty_the_theme_svg( 'cross' ); ?>
 				</button><!-- .nav-toggle -->
 
 				<?php
@@ -28,29 +27,29 @@
 				$mobile_menu_location = '';
 
 				// If the mobile menu location is not set, use the primary and expanded locations as fallbacks, in that order.
-				if (has_nav_menu('mobile')) {
+				if ( has_nav_menu( 'mobile' ) ) {
 					$mobile_menu_location = 'mobile';
-				} elseif (has_nav_menu('primary')) {
+				} elseif ( has_nav_menu( 'primary' ) ) {
 					$mobile_menu_location = 'primary';
-				} elseif (has_nav_menu('expanded')) {
+				} elseif ( has_nav_menu( 'expanded' ) ) {
 					$mobile_menu_location = 'expanded';
 				}
 
-				if (has_nav_menu('expanded')) {
+				if ( has_nav_menu( 'expanded' ) ) {
 
 					$expanded_nav_classes = '';
 
-					if ('expanded' === $mobile_menu_location) {
+					if ( 'expanded' === $mobile_menu_location ) {
 						$expanded_nav_classes .= ' mobile-menu';
 					}
 
-				?>
+					?>
 
-					<nav class="expanded-menu<?php echo esc_attr($expanded_nav_classes); ?>" aria-label="<?php echo esc_attr_x('Expanded', 'menu', 'twentytwenty'); ?>">
+					<nav class="expanded-menu<?php echo esc_attr( $expanded_nav_classes ); ?>" aria-label="<?php echo esc_attr_x( 'Expanded', 'menu', 'twentytwenty' ); ?>">
 
 						<ul class="modal-menu reset-list-style">
 							<?php
-							if (has_nav_menu('expanded')) {
+							if ( has_nav_menu( 'expanded' ) ) {
 								wp_nav_menu(
 									array(
 										'container'      => '',
@@ -65,45 +64,47 @@
 
 					</nav>
 
-				<?php
+					<?php
 				}
 
-				if ('expanded' !== $mobile_menu_location) {
-				?>
+				if ( 'expanded' !== $mobile_menu_location ) {
+					?>
 
-					<nav class="mobile-menu" aria-label="<?php echo esc_attr_x('Mobile', 'menu', 'twentytwenty'); ?>">
+					<nav class="mobile-menu" aria-label="<?php echo esc_attr_x( 'Mobile', 'menu', 'twentytwenty' ); ?>">
 
 						<ul class="modal-menu reset-list-style">
 
-							<?php
-							if ($mobile_menu_location) {
+						<?php
+						if ( $mobile_menu_location ) {
 
-								wp_nav_menu(
-									array(
-										'container'      => '',
-										'items_wrap'     => '%3$s',
-										'show_toggles'   => true,
-										'theme_location' => $mobile_menu_location,
-									)
-								);
-							} else {
+							wp_nav_menu(
+								array(
+									'container'      => '',
+									'items_wrap'     => '%3$s',
+									'show_toggles'   => true,
+									'theme_location' => $mobile_menu_location,
+								)
+							);
 
-								wp_list_pages(
-									array(
-										'match_menu_classes' => true,
-										'show_toggles'       => true,
-										'title_li'           => false,
-										'walker'             => new TwentyTwenty_Walker_Page(),
-									)
-								);
-							}
-							?>
+						} else {
+
+							wp_list_pages(
+								array(
+									'match_menu_classes' => true,
+									'show_toggles'       => true,
+									'title_li'           => false,
+									'walker'             => new TwentyTwenty_Walker_Page(),
+								)
+							);
+
+						}
+						?>
 
 						</ul>
 
 					</nav>
 
-				<?php
+					<?php
 				}
 				?>
 
@@ -111,9 +112,9 @@
 
 			<div class="menu-bottom">
 
-				<?php if (has_nav_menu('social')) { ?>
+				<?php if ( has_nav_menu( 'social' ) ) { ?>
 
-					<nav aria-label="<?php esc_attr_e('Expanded Social links', 'twentytwenty'); ?>">
+					<nav aria-label="<?php esc_attr_e( 'Expanded Social links', 'twentytwenty' ); ?>">
 						<ul class="social-menu reset-list-style social-icons fill-children-current-color">
 
 							<?php
