25db6581cdaa70ad4bdde6818fef7569cd2b0b33
khong <khong@DESKTOP-LVM7GK1>
2022-10-28 08:07:29 +0700

    module 8

--- wp-content/themes/twentytwenty/comments.php
+++ wp-content/themes/twentytwenty/comments.php
@@ -98,8 +98,10 @@ if ( $comments ) {
 	</div><!-- comments -->
 
 	<?php
-}
+}?>
 
+		<!-- đây là  make a post -->
+<?php
 if ( comments_open() || pings_open() ) {
 
 	if ( $comments ) {
@@ -130,3 +132,35 @@ if ( comments_open() || pings_open() ) {
 
 	<?php
 }
+?>
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script> -->
+<!-- <section class="card">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts" aria-selected="true">Make
+                                    a Post</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
+                                <div class="form-group">
+                                    <label class="sr-only" for="message">post</label>
+                                    <textarea class="form-control" id="message" rows="3" placeholder="What are you thinking..."></textarea>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="text-right">
+                        	<button type="submit" class="btn btn-primary">share</button>
+                        </div>
+                    </div>
+                </section> -->
+
+
+
+				
